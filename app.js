57ec32262388ac90159776c186628a30dcaa03e0
RakeshSuvvari <rakeshsuvvari3@gmail.com>
2022-06-27 13:11:34 +0530

    ignore

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Welcome to the world of NodeJS")
+
+var massage = "Hello"
+
+console.log(massage)
+
+function display(){
+    return "display function is called!"
+}
+
+console.log(display())
